2c1d5bbab246aa7be2b40ff9f6deb4f6bc28699b
Moondew <58519807+moon-dew@users.noreply.github.com>
2023-03-09 23:18:22 +0000

    Implemented change to item frame

--- Frames/item.py
+++ Frames/item.py
@@ -1,68 +1,105 @@
 import tkinter as tk
 from tkinter import ttk
+import json
+
 
 class Item(ttk.Frame):
-    def __init__(self, container, name, genre, score):
+
+    def __init__(self, container, id):
         super().__init__(container)
         # field options
         options = {'padx': 5, 'pady': 5}
         self.container = container
 
-        self.title = ttk.Label(self, text=name)
+        anilist = json.load(open("animelist.json"))
+        print([i for i in anilist['Accounts'][0]["List"]])
+        print(id)
+        self.item = [
+            i for i in anilist['Accounts'][0]["List"] if i[0] == int(id)
+        ][0]
+        masterlist = json.load(open("masterlist.json"))
+        self.masteritem = [
+            i for i in masterlist['List'] if i["ID"] == int(id)
+        ][0]
+        self.title = ttk.Label(self, text=self.masteritem["Name"])
         self.title.pack(**options)
+
         def quit():
             self.tabID = self.container.index("current")
             self.container.forget(self.tabID)
-        
+
         self.settings = Settings(self)
         self.settings.pack(anchor=tk.NW, **options)
         self.quit_button = ttk.Button(self, text="Quit", command=quit)
         self.quit_button.pack(side=tk.BOTTOM, **options)
+        self.scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
 
 class Settings(ttk.LabelFrame):
+
     def __init__(self, container):
         super().__init__(container, text="Settings")
         # field options
         options = {'padx': 5, 'pady': 5}
         self.container = container
+        self.item = self.container.item
+        self.masteritem = self.container.masteritem
 
+        values = [
+            "Watching", "Completed", "On Hold", "Dropped", "Plan to Watch"
+        ]
         self.init_status = tk.StringVar()
-        self.status = ttk.Combobox(self, values=["Watching", "Completed", "On Hold", "Dropped", "Plan to Watch"], textvariable=self.init_status, width=15, state="readonly")
-        self.status.current(0)
+        self.status = ttk.Combobox(self,
+                                   values=values,
+                                   textvariable=self.init_status,
+                                   width=15,
+                                   state="readonly")
+        self.status.current(self.item[1])
         self.status.pack(**options)
 
-       
         class Score(ttk.LabelFrame):
+
             def __init__(self, container):
                 super().__init__(container)
                 self.init_score = tk.StringVar()
-                self.score = ttk.Spinbox(self, from_=0, to=10, textvariable=self.init_score, state="readonly", width=2)
+                self.score = ttk.Spinbox(self,
+                                         from_=0,
+                                         to=10,
+                                         textvariable=self.init_score,
+                                         state="readonly",
+                                         width=2)
                 self.init_score.set("10")
                 self.score.pack(side=tk.LEFT, **options)
                 self.max_score = ttk.Label(self, text="/10")
-                self.max_score.pack(side=tk.RIGHT,**options)
+                self.max_score.pack(side=tk.RIGHT, **options)
 
         class Progress(ttk.LabelFrame):
+
             def __init__(self, container):
                 super().__init__(container)
-                episodes = 24
+                self.container = container
+                episodes = self.container.masteritem
                 self.init_progress = tk.StringVar()
-                self.progress = ttk.Spinbox(self, from_=0, to=episodes, textvariable=self.init_progress, state="readonly", width=4)
+                self.progress = ttk.Spinbox(self,
+                                            from_=0,
+                                            to=episodes,
+                                            textvariable=self.init_progress,
+                                            state="readonly",
+                                            width=4)
                 self.init_progress.set("10")
                 self.progress.pack(side=tk.LEFT, **options)
 
                 self.total_episodes = ttk.Label(self, text=f"/{episodes}")
                 self.total_episodes.pack(side=tk.RIGHT, **options)
-        
 
         self.score = Score(self)
         self.score.pack(anchor=tk.N, **options)
         self.progress = Progress(self)
         self.progress.pack(anchor=tk.S, **options)
 
+        class AddRemove(ttk.LabelFrame):
 
-        class AddRemove(ttk.LabelFrame):   
             def __init__(self, container):
                 super().__init__(container)
                 self.add_button = ttk.Button(self, text="+")
@@ -72,7 +109,3 @@ class Settings(ttk.LabelFrame):
 
         self.add_remove = AddRemove(self)
         self.add_remove.pack(**options)
-
-        #add scrollbar
-        self.scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL)
-        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
--- Frames/search.py
+++ Frames/search.py
@@ -5,7 +5,9 @@ import treeview_upgrade
 from tkinter import messagebox
 from Frames.item import Item
 
+
 class Search(ttk.Frame):
+
     def __init__(self, container):
         super().__init__(container)
         # field options
@@ -17,17 +19,26 @@ class Search(ttk.Frame):
         self.anime_list = json.load(open("masterlist.json"))
         self.search_bar.bind('<KeyRelease>', self.update)
 
-        headings = ("name", "genre", "score")
-        self.results = treeview_upgrade.MyTreeview(self, columns=headings, show="headings")
+        headings = ("name", "genre", "score", "id")
+        self.results = treeview_upgrade.MyTreeview(self,
+                                                   columns=headings,
+                                                   show="headings")
         self.results.heading("name", text="Name", sort_by="name")
         self.results.heading("genre", text="Genre", sort_by="name")
         self.results.heading("score", text="Average Score", sort_by="num")
+        self.results.heading("id", text="ID", sort_by='num')
+        self.results["displaycolumns"] = ("name", "genre", "score")
         j = self.anime_list
         for i in j["List"]:
-            self.results.insert("", "end", values=(i["Name"], i["Genre"], i['Rating']))
+            self.results.insert("",
+                                "end",
+                                values=(i["Name"], i["Genre"], i['Rating'],
+                                        i["ID"]))
         self.results.pack(**options)
         # add a scrollbar
-        self.scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.results.yview)
+        self.scrollbar = ttk.Scrollbar(self,
+                                       orient=tk.VERTICAL,
+                                       command=self.results.yview)
         self.results.configure(yscroll=self.scrollbar.set)
         self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         self.results.bind('<<TreeviewSelect>>', self.item_selected)
@@ -39,12 +50,19 @@ class Search(ttk.Frame):
         for i in j["List"]:
             if self.search_bar.get().lower() in i["Name"].lower():
                 if i["Name"].lower().startswith(self.search_bar.get().lower()):
-                    self.results.insert("", "end", values=(i["Name"], i["Genre"], i['Rating']))
+                    self.results.insert("",
+                                        "end",
+                                        values=(i["Name"], i["Genre"],
+                                                i['Rating']))
                 else:
                     matches.append(i)
         for i in matches:
-            self.results.insert("", "end", values=(i["Name"], i["Genre"], i['Rating']))
+            self.results.insert("",
+                                "end",
+                                values=(i["Name"], i["Genre"], i['Rating']))
 
     def item_selected(self, event):
         item = self.results.selection()[0]
-        self.container.add(Item(self.container, self.results.item(item, "values")[0], self.results.item(item, "values")[1], self.results.item(item, "values")[2]), text="Item")
+        self.container.add(Item(self.container,
+                                self.results.item(item, "values")[3]),
+                           text="Item")
